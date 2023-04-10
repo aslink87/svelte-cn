5df3c63f03e16f06ad81e2c120f0b85bc33c8ae9
aslink87 <adam.slinkman@gmail.com>
2023-04-10 16:08:10 -0400

    refactor(footer): svg and style work on footer

--- src/components/footer/Footer.svelte
+++ src/components/footer/Footer.svelte
@@ -3,133 +3,152 @@
   import { page } from '$app/stores';
 </script>
 
-<section data-testid="footer-wrapper">
-  <div class="left-container">
-    <h2>Address</h2>
-    <p>282 12th Street Plainwell, MI 49080 PO Box 53</p>
-    <h2>Phone</h2>
-    <p>1-269-685-4166</p>
-    <h2>Fax</h2>
-    <p>1-269-685-4178</p>
-  </div>
-  <div class="center-container">
-    <div class="icon-wrapper">
-      <a
-        href="https://www.facebook.com/ChristianNeighborsPlainwell/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512"
-          ><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-            d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"
-          /></svg
-        >
-      </a>
-      <a
-        href="https://www.instagram.com/christian_neighbors/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 448 512"
-          ><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-            d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
-          /></svg
-        >
-      </a>
+<section data-testid="footer">
+  <div class="svg">
+    <div class="footer-wrapper">
+      <div class="left-container">
+        <div class="icon-wrapper">
+          <a
+            href="https://www.facebook.com/ChristianNeighborsPlainwell/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512"
+              ><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+                d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"
+              /></svg
+            >
+          </a>
+          <a
+            href="https://www.instagram.com/christian_neighbors/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 448 512"
+              ><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+              /></svg
+            >
+          </a>
+        </div>
+        <p>Christian Neighbors, ©2023</p>
+      </div>
+      <div class="center-container">
+        <div>
+          <h2>Address</h2>
+          <p>282 12th Street Plainwell, MI 49080 PO Box 53</p>
+          <h2>Phone</h2>
+          <p>1-269-685-4166</p>
+          <h2>Fax</h2>
+          <p>1-269-685-4178</p>
+        </div>
+        <div>
+          <h2>Hours</h2>
+          <p><strong>Mon:</strong> 10-Noon, 4-6</p>
+          <p><strong>Tue:</strong> 10-Noon</p>
+          <p><strong>Wed:</strong> Closed</p>
+          <p><strong>Thu:</strong> 2-5</p>
+          <p><strong>Fri:</strong> Closed</p>
+          {#if $page.data.session?.user}
+            <button on:click={() => signOut()}>
+              <p>Logout</p>
+            </button>
+            <p class="welcome">Thanks for logging in {$page.data.session.user.name}</p>
+          {:else}
+            <button
+              on:click={() => signIn('google')}
+              class="cursor-pointer btn variant-filled-primary"
+            >
+              <p>Login</p>
+            </button>
+          {/if}
+        </div>
+      </div>
     </div>
-    <p>Christian Neighbors, ©2023</p>
-  </div>
-  <div class="right-container">
-    <h2>Hours</h2>
-    <p><strong>Mon:</strong> 10-Noon, 4-6</p>
-    <p><strong>Tue:</strong> 10-Noon</p>
-    <p><strong>Wed:</strong> Closed</p>
-    <p><strong>Thu:</strong> 2-5</p>
-    <p><strong>Fri:</strong> Closed</p>
-    {#if $page.data.session?.user}
-      <button on:click={() => signOut()}>
-        <p>Logout</p>
-      </button>
-      <p class="welcome">Thanks for logging in {$page.data.session.user.name}</p>
-    {:else}
-      <button on:click={() => signIn('google')} class="cursor-pointer btn variant-filled-primary">
-        <p>Login</p>
-      </button>
-    {/if}
   </div>
 </section>
 
 <style lang="scss">
   section {
-    display: flex;
-    justify-content: space-between;
-    gap: 2rem;
-    background-color: $white;
-    color: $dark-blue;
-    border-top: solid 2px $deep-blue;
-    padding: 2rem;
+    background-color: $deep-blue;
 
-    .left-container {
-      flex: 30%;
-    }
-
-    .center-container {
-      @include center;
-      flex: 30%;
+    .svg {
+      background-image: url('/images/wavesHand.png');
+      aspect-ratio: 2100/300;
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
       display: flex;
-      flex-wrap: wrap;
-      padding: 2rem 0;
+      position: relative;
 
-      p {
+      .footer-wrapper {
         width: 100%;
-        margin: 0;
-        font-size: 12px;
-      }
-
-      .icon-wrapper {
-        margin-bottom: 1rem;
+        margin: 0 2rem;
         display: flex;
-        gap: 1rem;
+        justify-content: space-between;
+        color: $dark-blue;
 
-        .icon {
-          width: 3rem;
-          fill: $dark-blue;
+        .left-container {
+          display: flex;
+          align-self: baseline;
+          flex-wrap: wrap;
+          margin: auto 0 1rem 0;
 
-          &:hover {
-            fill: $yellow;
-            transition: all 0.2s ease-out;
+          p {
+            width: 100%;
+            margin: 0;
+          }
+
+          .icon-wrapper {
+            margin-bottom: 1rem;
+            display: flex;
+            gap: 1rem;
+
+            .icon {
+              width: 3rem;
+              fill: $dark-blue;
+
+              &:hover {
+                fill: $yellow;
+                transition: all 0.2s ease-out;
+              }
+            }
           }
         }
-      }
-    }
+        .center-container {
+          margin: auto 10% 1rem 0;
+          display: flex;
+          flex-wrap: wrap;
+          flex-flow: row;
+          gap: 2rem;
 
-    .right-container {
-      flex: 30%;
-      text-align: right;
+          button {
+            background: none;
+            border: none;
+            color: $dark-blue;
+            cursor: pointer;
 
-      button {
-        background: none;
-        border: none;
-        color: $dark-blue;
-        cursor: pointer;
+            p {
+              font-size: 12px;
+            }
+          }
+        }
 
         p {
-          font-size: 12px;
+          margin: auto 0;
+          font-size: 14px;
+        }
+
+        .welcome {
+          font-size: 14px;
+        }
+
+        h2 {
+          @include h2-primary;
+          margin-bottom: 0.3rem;
+          font-size: 18px;
         }
       }
-    }
-
-    p {
-      margin: auto 0;
-    }
-
-    .welcome {
-      font-size: 12px;
-    }
-
-    h2 {
-      @include h2-primary;
-      margin-bottom: 0.3rem;
     }
   }
 </style>
