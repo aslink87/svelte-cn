9a4ec63bf2c41dbe5df612ff6c6df6e165eddbf1
Adam Slinkman <adam.slinkman@gmail.com>
2024-02-29 14:28:56 -0500

    update node port number

--- node-server.js
+++ node-server.js
@@ -3,6 +3,7 @@ import { join } from 'desm';
 // eslint-disable-next-line import/extensions
 import { handler } from './build/handler.js';
 
+// Serve dynamic assets from the `uploads` directory
 const assetsPath = join(import.meta.url, './uploads');
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(express.static(assetsPath));
 // let SvelteKit handle everything else, including serving prerendered pages and static assets
 app.use(handler);
 
-app.listen(5173, () => {
-  console.log('listening on port 5173');
+app.listen(4173, () => {
+  console.log('listening on port 4173');
   console.log('assetsPath', assetsPath);
 });
