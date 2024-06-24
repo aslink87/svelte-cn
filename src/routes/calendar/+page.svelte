7c0e866c877f62c70b9305899762365cde646ef5
aslink87 <adam.slinkman@gmail.com>
2024-06-24 14:41:20 -0400

    move events route to calendar

---
+++ src/routes/calendar/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { seo } from '$lib/stores/Seo';
+  import Seo from '../SEO.svelte';
+
+  seo.set({
+    title: 'CN - Calendar',
+    description: 'Christian Neighbors - Calendar',
+  });
+</script>
+
+<Seo title={$seo.title} description={$seo.description} />
+
+<section class="calendar center component pt-8" data-testid="calendar">
+  <h1 class="h1-primary">Upcoming Events</h1>
+</section>
