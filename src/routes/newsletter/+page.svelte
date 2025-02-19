fa612c8bfc9632fcf54d35574c190b4bbd90dab8
aslink87 <adam.slinkman@gmail.com>
2025-02-19 12:25:42 -0500

    add spinner, delay loading newsletter pdf until src is fetched

--- src/routes/newsletter/+page.svelte
+++ src/routes/newsletter/+page.svelte
@@ -2,8 +2,17 @@
   import { seo } from '$lib/stores/Seo';
   import Seo from '../SEO.svelte';
   import type { NewsletterType } from '$/types';
+  import Spinner from '$/components/utility/Spinner.svelte';
 
   export let data: { newsletters: NewsletterType[] };
+  let isLoading = true;
+
+  const delayRender = () => {
+    setTimeout(() => {
+      isLoading = false;
+    }, 4000);
+  };
+  delayRender();
 
   seo.set({
     title: 'CN - Newsletter',
@@ -13,20 +22,34 @@
   const enews = data.newsletters.filter((n) => n.index === 0).at(0);
   const firstNewsletter = data.newsletters.filter((n) => n.index === 1).at(0);
   const secondNewsletter = data.newsletters.filter((n) => n.index === 2).at(0);
+
+  const env: string = import.meta.env.MODE;
+  let pdfUrl: string;
+  if (env === 'development') {
+    pdfUrl =
+      'https://christianneighbors.org/assets/newsletters/a635e2f0-e4ab-4955-be6e-ed3ef95e532e.pdf';
+  } else if (enews) {
+    pdfUrl = `https://docs.google.com/gview?url=${import.meta.env.VITE_PUBLIC_ASSET_URL}${
+      enews.doc
+    }&embedded=true`;
+  }
 </script>
 
 <Seo title={$seo.title} description={$seo.description} />
 <section class="newsletter center component pt-8" data-testid="newsletter">
   <h1 class="h1-primary">What's Happening</h1>
-  {#if enews}
+  {#if isLoading}
+    <div class="center flex h-[25em] sm:h-[55em]">
+      <Spinner />
+    </div>
+  {/if}
+  {#if enews && !isLoading}
     <div class="e-news mx-auto my-8 w-full max-w-[60em] sm:w-[90%]">
       <h2 class="h2-primary mt-8 capitalize">{enews.title}</h2>
       <iframe
         class="mx-auto mt-8 h-[25em] w-full sm:h-[55em] sm:w-[80%]"
         title="E-News Preview"
-        src={`https://docs.google.com/gview?url=${import.meta.env.VITE_PUBLIC_ASSET_URL}${
-          enews.doc
-        }&embedded=true`}
+        src={`https://docs.google.com/gview?url=${pdfUrl}&embedded=true`}
       ></iframe>
     </div>
   {/if}
